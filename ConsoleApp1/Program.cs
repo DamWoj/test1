fc957aa9e498c434fc027073a5efef0df1922ea4
Damian <dam.woj90@gmail.com>
2021-06-22 23:09:37 +0200

    initial
    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        async static Task<List<Record>> FetchFlats()
+        {
+            var flats = new List<Record>();
+            using (var httpClient = new HttpClient())
+            {
+                using var response = await httpClient.GetAsync(@"http://net-poland-interview-stretto.us-east-2.elasticbeanstalk.com/api/flats/csv");
+                {
+                    response.EnsureSuccessStatusCode();
+                    using var streamReader = new StreamReader(await response.Content.ReadAsStreamAsync());
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            Console.WriteLine("1. Flats: ");
+                            var line = await streamReader.ReadLineAsync();
+                            Console.WriteLine(line);
+                            flats.Add(new Record(line));
+                        }
+                    }
+                }
+            }
+            return flats;
+        }
+
+
+
+        async static Task Main(string[] args)
+        {
+            var listOfFlats = new List<Record>();
+            try
+            {
+                listOfFlats = await FetchFlats();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error with fetching flats.");
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Closing program");
+            }
+
+            if (!listOfFlats.Any())
+            {
+                Console.WriteLine("Flats are empty. Closing program.");
+                return;
+            }
+            listOfFlats.RemoveAt(0); // removing headers
+            WaitForKeyAndClearScreen();
+            Console.WriteLine("2. Objects of flats: ");
+
+            listOfFlats.ForEach(c => Console.WriteLine(JsonSerializer.Serialize(c, typeof(Record))));
+
+            WaitForKeyAndClearScreen();
+            Console.WriteLine("3. Biggest flats by City: ");
+            var GroupByCity = listOfFlats.Where(c => c.Type == "Residential").GroupBy(c => c.City);
+            foreach (var group in GroupByCity)
+            {
+                Console.WriteLine(group.Aggregate((a, b) => a.SqFt > b.SqFt ? a : b).ToString());
+            }
+
+            WaitForKeyAndClearScreen();
+            Console.WriteLine("4. Biggest cheapest flats: ");
+
+            Console.WriteLine(listOfFlats.OrderByDescending(c => c.Baths + c.Beds).ThenBy(c => c.Price).First().ToString());
+
+            WaitForKeyAndClearScreen();
+            Console.WriteLine("5. Most expensive flat with tax by City: ");
+            List<Record> mostExpensiveFlatsWithTaxByCities = new List<Record>();
+            foreach (var group in GroupByCity)
+            {
+                var mostExpensiveFlatInCity = group.Aggregate((a, b) => a.Price > b.Price ? a : b);
+            }
+
+            WaitForKeyAndClearScreen();
+        }
+
+        private static void WaitForKeyAndClearScreen()
+        {
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ ConsoleApp1/Record.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Record
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string State { get; set; }
+        public string Type { get; set; }
+        public int Beds { get => _beds; set => _beds = value; }
+        private int _beds;
+        public int Baths { get => _baths; set => _baths = value; }
+        private int _baths;
+        public double SqFt { get => _sqFt; set => _sqFt = value; }
+        private double _sqFt;
+        public DateTime SaleDate { get => _saleDate; set => _saleDate = value; }
+        private DateTime _saleDate;
+        public decimal Price { get => _price; set => _price = value; }
+        private decimal _price;
+        public double Latitude { get => _latitude; set => _latitude = value; }
+        private double _latitude;
+        public double Longitude { get => _longitude; set => _longitude = value; }
+        private double _longitude;
+        public Record(string line)
+        {
+            var columns = line.Split(',');
+            Street = columns[0] ?? string.Empty;
+            City = columns[1] ?? string.Empty;
+            Zip = columns[2] ?? string.Empty;
+            State = columns[3] ?? string.Empty;
+            Type = columns[7] ?? string.Empty;
+            int.TryParse(columns[4], out _beds);
+            int.TryParse(columns[5], out _baths);
+            double.TryParse(columns[6], out _sqFt);
+            DateTime.TryParse(columns[8], out _saleDate);
+            decimal.TryParse(columns[9],out _price);
+            double.TryParse(columns[10], out _latitude);
+            double.TryParse(columns[11], out _longitude);
+        }
+
+        public override string ToString()
+        {
+            return $"street: {Street} city: {City} zip: {Zip} state: {State} type: {Type} beds: {Beds} baths: {Baths} sq__ft: {SqFt} sale_date: {SaleDate} price: {Price} latitude: {Latitude} longitude: {Longitude}";
+        }
+
+
+    }
+}
